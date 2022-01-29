98a4c685a19f88160a2e87530060de699c51d004
ujin <dklvmf0106@gmail.com>
2022-01-29 11:46:33 +0900

    sort - 버블, 선택, 삽입

--- heap/q5.py
+++ heap/q5.py
@@ -2,29 +2,30 @@
 import heapq
 
 def solution(jobs):
-    answer, now, i = 0, 0, 0
+    answer = 0
+    end, i = 0, 0
     start = -1
     heap = []
 
     # jobs의 길이 만큼 반복
     while i < len(jobs):
         for j in jobs:
-            # start < jobs의 요소 index 1 < now 이면
-            if start < j[0] <= now:
+            # start < jobs의 요소 index 1 < end 이면
+            if start < j[0] <= end:
                 # j의 요소를 반대로 힙에 푸시
                 heapq.heappush(heap, [j[1], j[0]])
         # 힙의 길이가 0 보다 크먄
         if len(heap) > 0:
-            # current 는 힙팝
-            current = heapq.heappop(heap)
-            # start에 now값 넣기
-            start = now
-            # now에 현재 pop된 요소의 인덱스 0(길이)
-            now += current[0]
+            # now 는 힙팝
+            now = heapq.heappop(heap)
+            # start에 end값 넣기
+            start = end
+            # end에 현재 pop된 요소의 인덱스 0(길이)
+            end += now[0]
             # answer 에 (현재 위치 - 현재요소의 작업 요청시간)을 더한다
-            answer += now - current[1]
+            answer += end - now[1]
             i += 1
         else:
-            now += 1
+            end += 1
     # 모든 요청에서 종료까지 시간을 다해 갯수만틈 나누기
     return int(answer / len(jobs))
