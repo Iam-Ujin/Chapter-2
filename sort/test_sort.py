8b033156e57fb74d2b1181f015324f5b67516d62
ujin <dklvmf0106@gmail.com>
2022-02-01 18:44:27 +0900

    병합정렬

--- sort/test_sort.py
+++ sort/test_sort.py
@@ -1,4 +1,5 @@
 from sort.sorts import bubblesort, selectionsort, insertionsort_2, insertionsort
+from mergesort import mergesort
 
 assert bubblesort([4, 6, 2, 9, 1]) == [1, 2, 4, 6, 9]
 assert bubblesort([3, 2, 1, 5, 3, 2, 3]) == [1, 2, 2, 3, 3, 3, 5]
@@ -11,3 +12,6 @@ assert insertionsort([3, 2, 1, 5, 3, 2, 3]) == [1, 2, 2, 3, 3, 3, 5]
 
 assert insertionsort_2([4, 6, 2, 9, 1]) == [1, 2, 4, 6, 9]
 assert insertionsort_2([3, 2, 1, 5, 3, 2, 3]) == [1, 2, 2, 3, 3, 3, 5]
+
+assert mergesort([4, 6, 2, 9, 1]) == [1, 2, 4, 6, 9]
+assert mergesort([3, 2, 1, 5, 3, 2, 3]) == [1, 2, 2, 3, 3, 3, 5]
